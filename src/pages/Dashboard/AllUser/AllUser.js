b6d16b572b8ff28f871affc3a6681046e30aae06
mahin33ferdous <mahinferdousshoily@gmail.com>
2023-10-02 16:34:29 -1100

    created admin access permission for adding admin

--- src/pages/Dashboard/AllUser/AllUser.js
+++ src/pages/Dashboard/AllUser/AllUser.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 import { useQuery } from 'react-query';
 
 const AllUser = () => {
-    const {data:users=[]}= useQuery({
+    const {data:users=[],refetch}= useQuery({
         querykey:['users'],
         queryFn: async()=>{
             const res=await fetch(`http://localhost:5000/users`);
             const data= await res.json()
+
            
             //console.log(data)
             return data
            
         }
     });
+// updating admin role to user
+    const handleMakeAdmin=(id)=>{
+        fetch(`http://localhost:5000/users/admin/${id}`,{
+            method: 'PUT',
+            headers:{
+                authorization :`bearer ${localStorage.getItem('accessToken')}`//sendng access token to server side
+            }
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            if(data.modifiedCount>0)
+            {
+                toast.success("Admin created successfully")
+                refetch()
+            }
+        })
+           
+            
+    }
 
     return (
         <div>
@@ -36,7 +58,7 @@ const AllUser = () => {
         <th>{i}</th>
         <th>{user.name}</th>
         <th>{user.email}</th>
-        <th><button className='btn btn-xs btn-primary'>Make Admin</button></th>
+        <th>{user?.role !=='admin' && <button onClick={()=>handleMakeAdmin(user._id)} className='btn btn-xs btn-primary'>Make Admin</button>}</th>
         <th><button className='btn btn-xs btn-danger'>Delete</button></th>
         
 
